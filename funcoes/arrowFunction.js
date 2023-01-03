2ec1919581812ce2d50cd4191835818bf0b1c9ad
Amanda Allis <104679365+amandaallis@users.noreply.github.com>
2023-01-03 11:07:30 -0300

    commit utilizando return na arrowFunction

--- funcoes/arrowFunction.js
+++ funcoes/arrowFunction.js
@@ -27,3 +27,25 @@ function multiplica(numero1, numero2){
 let multiplica= (n1, n2) => `a multiplicação de ${n1} X ${n2} é ${n1*n2}` 
 imprimeTexto(multiplica(2,1))
 
+/*ATENÇÃO: É importante ressaltar que se tiver mais de uma linha no código haverá a necessidade de colocar o return
+
+ex:
+function somaNumerosPequenos (num1, num2){
+    if (num1>10 || num2 >10 ){
+        return "Digite somente números de 1 a 9"
+    }
+    else{
+        return num1+num2
+    }
+}
+imprimeTexto(somaNumerosPequenos(2, 1))
+*/
+const somaNumerosPequenos = (num1, num2) => {
+    if (num1>10 || num2 >10 ){
+        return "Digite somente números de 1 a 9"
+    }
+    else{
+        return num1+num2
+    }
+}
+imprimeTexto(somaNumerosPequenos(2,4))
